5536e0ac0c7602c95f7056cbbf5bae2ba5dbedfb
Tesfa <natnaelmalike@gmail.com>
2023-02-24 05:21:27 +0300

    edititng

--- order.js
+++ order.js
@@ -25,3 +25,31 @@ searchBtn.addEventListener('click', function() {
   }
   
 });
+
+class Order {
+  constructor(name, email, number, city, woreda, home, comment, payment) {
+    this.name = name;
+    this.email = email;
+    this.number = number;
+    this.city = city;
+    this.woreda = woreda;
+    this.home = home;
+    this.comment = comment;
+    this.payment = payment;
+  }
+}
+const form = document.querySelector('#myForm');
+form.addEventListener('submit', function() {
+  
+  const name = document.querySelector('#name').value;
+  const email = document.querySelector('#email').value;
+  const number = document.querySelector('#phoneNumber').value;
+  const city = document.querySelector('#city').value;
+  const woreda = document.querySelector('#state').value;
+  const home = document.querySelector('#Street').value;
+  const comment = document.querySelector('#comment').value;
+  const payment = document.querySelector('input[name="pay"]:checked').value;
+  const formData = new Order(name,email,number,city,woreda,home,comment,payment)
+  localStorage.setItem('formData', JSON.stringify(formData));
+  alert('Form data saved!');
+});
