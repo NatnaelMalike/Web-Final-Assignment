32a392398f34022668d4d65bf4a778619ad4acff
Tesfa Mk <natnaelmalike@gmail.com>
2023-02-24 11:22:17 +0300

    editing

--- order.js
+++ order.js
@@ -7,11 +7,22 @@ function showDiv(text) {
   }
 }
 const books = [
-  { title: 'the', author: 'F. Scott Fitzgerald' },
-  { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
-  { title: '1984', author: 'George Orwell' },
-  { title: 'Pride and Prejudice', author: 'Jane Austen' },
-  { title: 'The Catcher in the Rye', author: 'J.D. Salinger' }
+  { title: 'cocktail', author: 'F. Scott Fitzgerald' },
+  { title: 'chips and dips', author: 'Harper Lee' },
+  { title: 'banoffe cake', author: 'George Orwell' },
+  { title: 'chicken', author: 'Jane Austen' },
+  { title: 'steak', author: 'J.D. Salinger' },
+  { title: 'fish', author: 'F. Scott Fitzgerald' },
+  { title: 'shrimp with pasta', author: 'Harper Lee' },
+  { title: 'shekla tebs', author: 'George Orwell' },
+  { title: 'tej', author: 'Jane Austen' },
+  { title: 'areke', author: 'J.D. Salinger' },
+  { title: 'white muffins', author: 'F. Scott Fitzgerald' },
+  { title: 'black forest', author: 'Harper Lee' },
+  { title: 'chocolate cupcake', author: 'George Orwell' },
+  { title: 'soup', author: 'Jane Austen' },
+  { title: 'chicken salad', author: 'J.D. Salinger' },
+  { title: 'doro wet', author: 'J.D. Salinger' }
 ];
 const searchInput = document.getElementById('searchInput');
 const searchBtn = document.getElementById('searchBtn');
